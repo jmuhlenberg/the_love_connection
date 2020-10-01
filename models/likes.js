aa0e76709356ec2764e35f02f57fe5363039db37
John Muhlenberg <jmuhlenberg16@yahoo.com>
2020-10-01 16:02:52 -0400

    user_controller.js all index, create, edit, delete done. app.js started. likes and user models updated to be connected

--- models/likes.js
+++ models/likes.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose')
 
 const likesSchema = new mongoose.Schema({
-  name: String,
+
   age: String,
   height: String,
   gender: String,
   build: String,
   eyeColor: String,
   hairColor: String
+
 })
 
-const likes = mongoose.model('likes', bookSchema)
+const likes = mongoose.model('likes', likesSchema)
 
 module.exports = likes
